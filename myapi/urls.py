133ca9361925db0eafa94b8c18978a450323625a
AyaMohamed1 <yoyomohamed102000@gmail.com>
2022-02-09 15:00:21 +0200

    second commit

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path,include
 # from .views import notser
 from rest_framework import routers
-from .views import IntakeList, StudentList
-from student_register.models import intake
+from .views import IntakeList, StudentList, deleteStudent
+from student_register.models import intake, Student
 
 router = routers.DefaultRouter()
 router.register(r'Student', StudentList)
@@ -10,5 +10,6 @@ router.register(r'intake', IntakeList)
 urlpatterns = [
     # path('', notser, name='notser'),
     path('', include(router.urls)),
+    path('delete/<id>', deleteStudent),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- myapi/views.py
+++ myapi/views.py
@@ -4,6 +4,9 @@ from html5lib import serialize
 from student_register.models import intake, Student
 from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import permissions
 from .serializers import IntakeSerializer, StudentsSerializer
 # Create your views here.
@@ -15,9 +18,18 @@ from .serializers import IntakeSerializer, StudentsSerializer
 #     #     pass
 #     return HttpResponse('not ser error!')
 
+
+@api_view(['POST'])
+def deleteStudent (request,pk):
+    student = Student.objects.get(pk=id)
+    student.delete()
+    return Response({'message': '{} student was deleted successfully!'.format(student.fullname)})
+
 class IntakeList(viewsets.ModelViewSet):
     queryset = intake.objects.all()
     serializer_class = IntakeSerializer
+
+
 
 class StudentList(viewsets.ModelViewSet):
     queryset = Student.objects.all()
